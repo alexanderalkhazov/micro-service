68d73ab14156140f08bf9117650c4005cc98df35
Alexander Alhazov <alexander410dev@gmail.com>
2024-04-12 17:10:04 +0300

    adding auth service

--- app.js
+++ app.js
@@ -2,4 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+
+
+app.route('/auth', require('./routes/account.route'));
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
